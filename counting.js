9b33b94e13b448459ea566b908aae9af6ec93017
mariajalmeida <mari9d91@stud.kea.dk>
2021-02-09 21:12:09 +0100

    halfway

--- counting.js
+++ counting.js
@@ -5,11 +5,14 @@ function actionBegins() {
 
     let number = 0;
     let speed = 100;
+    const array = [];
 
-    const int = setInterval(function () {
+    const int = setInterval(res => {
         number++;
-        if (number <= 20) {
-            console.log(number);
+        if (number <= 9) {
+            number = number.toString();
+            array.unshift(number);
+            console.log(array);
         } else {
             console.log("done");
             clearInterval(int);
