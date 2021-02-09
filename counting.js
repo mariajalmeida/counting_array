a091b51c565b5c410dab05f9ea7520cd9cf1642e
mariajalmeida <mari9d91@stud.kea.dk>
2021-02-09 18:05:17 +0100

    counting up and running

--- counting.js
+++ counting.js
@@ -3,4 +3,16 @@ window.addEventListener("DOMContentLoaded", actionBegins);
 function actionBegins() {
     console.log("sup");
 
+    let number = 0;
+    let speed = 100;
+
+    const int = setInterval(function () {
+        number++;
+        if (number <= 20) {
+            console.log(number);
+        } else {
+            console.log("done");
+            clearInterval(int);
+        }
+    }, speed);
 }
